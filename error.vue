50b2bb3f81949832467d7238c9cd20f3f22ba1a9
ksquaredcoding <ksquaredcoding@gmail.com>
2023-01-18 17:09:05 -0700

    added error for browser related routing

--- error.vue
+++ error.vue
@@ -3,12 +3,15 @@
     <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Ooops.</p>
     <p class="mt-7">{{ error.message }}</p>
+    <button class="btn my-7" @click="handleClearError">Go Home...</button>
   </div>
 </template>
 
 <script setup>
 
 defineProps(['error'])
+
+const handleClearError = () => clearError({ redirect: '/' })
 
 </script>
 
