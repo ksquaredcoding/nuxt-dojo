2cf035436274b31de4369c77778a3f9e36ad9da6
ksquaredcoding <ksquaredcoding@gmail.com>
2023-01-19 14:50:16 -0700

    nuxt api calls

--- pages/about.vue
+++ pages/about.vue
@@ -5,11 +5,14 @@
       earum. Possimus!</p>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio esse vel odit quo vero eius quam ab officiis
       earum. Possimus!</p>
+    <div>{{ data }}</div>
   </div>
 </template>
 
 
 <script setup>
+
+const { data } = await useFetch('/api/ninja')
 
 </script>
 
